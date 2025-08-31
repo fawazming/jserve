6bfc41b45f2628106ced52c6b8cbf8968344e9a4
fawazming <fawazming@gmail.com>
2025-08-31 07:49:02 +0100

    cors

---
+++ ci/app/Filters/Cors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class Cors implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+        // Handle preflight (OPTIONS) requests
+        if ($request->getMethod() === 'options') {
+            header("HTTP/1.1 200 OK");
+            exit; // stop execution for preflight
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        return $response;
+    }
+}
