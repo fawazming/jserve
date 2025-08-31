c62fefcc8ed6f4f597181caca5f19ba7914a67be
fawazming <fawazming@gmail.com>
2025-08-31 09:14:40 +0100

    con

--- ci/app/Controllers/SlugJson.php
+++ ci/app/Controllers/SlugJson.php
@@ -26,7 +26,6 @@ class SlugJson extends ResourceController
 
         // json_data is stored as string, decode it before sending
         $jsonData = json_decode($slugJson['json_data'], true);
-        header("Access-Control-Allow-Origin: *");
 
         return $this->respond($jsonData);
     }
@@ -53,15 +52,7 @@ class SlugJson extends ResourceController
         if (!$saved) {
             return $this->failServerError('Failed to save JSON data');
         }
-        // header("Access-Control-Allow-Origin: *");
-        // return $this->respondCreated(['message' => 'JSON data updated for slug: ' . $slug]);
-
-        $response = service('response');
-        // Set a custom header
-        $response->setHeader('Access-Control-Allow-Origin', '*');
-        // Set the body content
-        $response->setBody(['message' => 'JSON data updated for slug: ' . $slug]);
-
-        return $response;
+        header("Access-Control-Allow-Origin: *");
+        return $this->respondCreated(['message' => 'JSON data updated for slug: ' . $slug]);
     }
 }
